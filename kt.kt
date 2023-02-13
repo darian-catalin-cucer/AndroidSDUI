935f2460a05256cb5cb2f74e4d4517683dc19226
Darian-Cătălin Cucer <86965240+cucerdariancatalin@users.noreply.github.com>
2023-02-13 12:27:03 +0200

    Create kt.kt

---
+++ kt.kt
@@ -0,0 +1,13 @@
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.TextView
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        
+        val textView = findViewById<TextView>(R.id.text_view)
+        textView.text = "Hello, Android!"
+    }
+}
